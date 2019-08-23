3f70a78d7c6e66beb80409322317e608041c8938
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2019-08-23 15:55:59 -0500

    Update instruction.sh

--- static/instruction.sh
+++ static/instruction.sh
@@ -7,7 +7,7 @@ cat << WELCOME
 +-----------------------------------------------------------------------+
 |Thank you for downloading this Wordpress VM made by T&M Hansson IT AB! |
 |                                                                       |
-|This WordPress appliance comes with PHP 7.2, NGINX and Redis Cache for |
+|This WordPress appliance comes with PHP 7.3, NGINX and Redis Cache for |
 |best performance. The site(s) load very fast!                          |
 |                                                                       |
 |In order to configure everything you have to run the  setup script     |
@@ -15,7 +15,7 @@ cat << WELCOME
 |                                                                       |
 |Complete instructions can be found here: https://bit.ly/2E5fbUn        |
 |                                                                       |
-|If you have any questions please email me: daniel@hanssonit.se         |
+|If you have any questions please email me: duane.britting@gamil.com    |
 +-----------------------------------------------------------------------+
 
 WELCOME
