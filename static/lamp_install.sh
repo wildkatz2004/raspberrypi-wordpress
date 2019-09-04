5486ba2b75617bcefc632b4507321a19e1560673
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2019-09-04 11:44:21 -0500

    Update lamp_install.sh

--- static/lamp_install.sh
+++ static/lamp_install.sh
@@ -38,13 +38,10 @@ chown root:root $MYCNF
 install_mariadb(){
 
 # Install MARIADB
-apt install software-properties-common -y
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://ftp.ddg.lth.se/mariadb/repo/10.2/ubuntu xenial main'
-sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password password $MARIADB_PASS"
-sudo debconf-set-selections <<< "mariadb-server-10.2 mysql-server/root_password_again password $MARIADB_PASS"
+sudo debconf-set-selections <<< "mariadb-server-10.3 mysql-server/root_password password $MARIADB_PASS"
+sudo debconf-set-selections <<< "mariadb-server-10.3 mysql-server/root_password_again password $MARIADB_PASS"
 apt update -q4 & spinner_loading
-check_command apt install mariadb-server -y
+check_command apt install mariadb-server mariadb-client -y
 
 # Prepare for MySQL user updates
 log "Info" "Updating mysql user..."
@@ -56,7 +53,6 @@ log "Info" "Mysql user updates completed..."
 
 # mysql_secure_installation
 apt -y install expect
-log "Info" "Spawning mysql_secure_installation..."
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
@@ -76,6 +72,7 @@ expect eof
 ")
 echo "$SECURE_MYSQL"
 apt -y purge expect
+
 log "Info" "mysql_secure_installation config. completed..."
 # Write a new MariaDB config
 log "Info" "Preparing to create new mycnf file..."
@@ -83,94 +80,24 @@ run_static_script new_etc_mycnf
 log "Info" "Creation of new mycnf file completed..."
 }
 
-apache_check_module() {
-	echo "Checking for ${2} ${1}.${3}"
-	if [ ! -f "/etc/apache2/mods-${2}/${1}.${3}" ]
-	then
-		echo "Not found"
-		return 1
+# Install Nginx
+install_nginx(){
+apt update -q4 && spinner_loading
+	if check_sys packageManager apt; then
+		apt_nginx_package=(
+		nginx
+		)
+		log "Info" "Starting to install primary packages for NGINX..."
+		for depend in ${apt_nginx_package[@]}
+		do
+		    error_detect_depends "apt-get -y install ${depend}"
+		done
+		log "Info" "Install primary packages for NGINX completed..."
 	fi
-	echo "Found, checking for loading directives"
-	if [ "$3" == "load" ]
-	then
-		result=$(grep -E "^[^#]*LoadModule\\s*${1}_module" "/etc/apache2/mods-${2}/${1}.${3}")
- 		if [ -z "$result" ]
-		then
-			"Loading directive not found"
-			return 1
-		fi
-	fi
-	echo "Detected ${2} ${1}.${3} configuration, setting up integration"
-	return 0
-}
-
-apache_force_enable_module() {
-	echo "Force enabling module ${1}.${2}"
-	if [ ! -f "/etc/apache2/mods-available/${1}.${2}" ]
-	then
-		echo "WARNING: Unsupported ${1}, not configuring"
-		return 1
-	fi
-	echo "Available module found"
-	rm -f "/etc/apache2/mods-enabled/${1}.${2}"
-	echo "Removed possible duplicates"
-	apache_enable_module $1 $2
-	echo "Completed force enabling"
-	return 0
-}
-
-apache_enable_module() {
-	echo "Enabling module ${1}.${2}"
-	if [ ! -f "/etc/apache2/mods-available/${1}.${2}" ]
-	then
-		echo "WARNING: Could not enable ${1}, not configuring"
-		return 1
-	fi
-	echo "Found available module"
-	if [ -f "/etc/apache2/mods-enabled/${1}.${2}" ]
-	then
-		echo "Module already enabled"
-		return 0
-	fi
-	echo "Creating a symlink"
-	ln -s "../mods-available/${1}.${2}" "/etc/apache2/mods-enabled/${1}.${2}"
-	echo "Finished creating a symlink"
-	return 0
-}
-# Install Apache Function
-install_apache_depends(){
-
-    #Install Apache dependencies
-    log "Info" "Starting to install dependencies packages for Apache..."
-    local apt_list=(openssl libssl-dev libxml2-dev lynx lua-expat-dev libjansson-dev)
-
-    if check_sys packageManager apt; then
-        for depend in ${apt_list[@]}; do
-            error_detect_depends "apt-get -y install ${depend}"
-        done
-    fi
-    log "Info" "Install dependencies packages for Apache completed..."
-}
-
-
-install_apache(){
-
-	#Install Apache
-	check_command apt-get install -y apache2 apache2-utils libapache2-mod-fastcgi
-	#Enable Modules and Make Apache Config changes
-	sudo a2dismod mpm_worker mpm_prefork
-	#sudo a2enmod mpm_event rewrite ssl actions include cgi actions fastcgi alias proxy_fcgi fastcgi
-	sudo a2enmod mpm_event alias rewrite fastcgi expires headers ssl actions include proxy_fcgi
-	#configure_apache
-
-	log "Info" "Attempting tweaks to /etc/apache2/conf-available/security.conf"
-	#Tweak Apache settings - let's hide what OS and Webserver this server is running	    
-	sed -i "s/ServerTokens OS/ServerTokens Prod/" /etc/apache2/conf-available/security.conf
-	sed -i "s/ServerSignature On/ServerSignature Off/" /etc/apache2/conf-available/security.conf	    
-	log "Info" "Tweaks completed"
-	#Restart Apache
-	sudo systemctl restart apache2
 	
+sudo systemctl stop nginx.service
+sudo systemctl start nginx.service
+sudo systemctl enable nginx.service
 }
 
 # Install PHP Dependencies Function
@@ -178,11 +105,7 @@ install_php_depends(){
 
     if check_sys packageManager apt; then
         apt_depends=(
-            autoconf patch m4 bison libbz2-dev libgmp-dev libicu-dev libldb-dev libpam0g-dev
-            libldap-2.4-2 libldap2-dev libsasl2-dev libsasl2-modules-ldap libc-client2007e-dev libkrb5-dev
-            autoconf2.13 pkg-config libxslt1-dev zlib1g-dev libpcre3-dev libtool unixodbc-dev libtidy-dev
-            libjpeg-dev libpng-dev libfreetype6-dev libpspell-dev libmhash-dev libenchant-dev libmcrypt-dev
-            libcurl4-gnutls-dev libwebp-dev libxpm-dev libvpx-dev libreadline-dev snmp libsnmp-dev python-software-properties
+            autoconf patch m4 snmp python-software-properties
         )
         log "Info" "Starting to install dependencies packages for PHP..."
         for depend in ${apt_depends[@]}
@@ -194,48 +117,58 @@ install_php_depends(){
 	
 }
 
-#create mysql cnf
-create_php_fastcgi_conf(){
-log "Info" "Beginning creation of php-fpm.conf"
-cat > /etc/apache2/mods-enabled/fastcgi.conf << EOF 
-
-<IfModule mod_fastcgi.c>
-  AddHandler php7-fcgi-sites .php
-  Action php7-fcgi-sites /php7-fcgi-sites
-  Alias /php7-fcgi-sites /usr/lib/cgi-bin/php7-fcgi-sites
- FastCgiExternalServer /usr/lib/cgi-bin/php7-fcgi-sites -idle-timeout 60 -socket /var/run/php/php7.3-fpm.sock -pass-header Authorization
-
-  <Directory /usr/lib/cgi-bin>
-    Require all granted
-  </Directory>
-</IfModule>	
-EOF
-log "Info" "Completed creation of $phptoinstall-fpm.conf"
-log "Info" "create $phptoinstall-fpm.conf file at /etc/apache2/conf-available/php-fpm.conf completed."
-
-}
-
 # Configure PHP Function
 configure_php(){
 log "Info" "Beginning php.ini edits."
+	# Make file backups
+	sudo cp /etc/php/7.3/fpm/pool.d/www.conf /etc/php/$php_ver_num/fpm/pool.d/www.conf.bak
+	sudo cp /etc/php/7.3/cli/php.ini /etc/php/$php_ver_num/cli/php.ini.bak
+	sudo cp /etc/php/7.3/fpm/php.ini /etc/php/$php_ver_num/fpm/php.ini.bak
+	sudo cp /etc/php/7.3/fpm/php-fpm.conf /etc/php/$php_ver_num/fpm/php-fpm.conf.bak
+	sudo cp /etc/php/7.3/fpm/pool.d/www.conf /etc/php/$php_ver_num/fpm/pool.d/www.conf.bak
+	sudo cp /etc/php/7.3/cli/php.ini /etc/php/$php_ver_num/cli/php.ini.bak
 	# Configure PHP
-	sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$php_ver_num/cli/php.ini
-	sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$php_ver_num/cli/php.ini
-	sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/$php_ver_num/cli/php.ini
-	sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/$php_ver_num/cli/php.ini
-	# Configure PHP-FPM
-	sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$php_ver_num/cli/php.ini
-	sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$php_ver_num/cli/php.ini
-	sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s/upload_max_filesize = .*/upload_max_filesize = 256M/" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s/post_max_size = .*/post_max_size = 256M/" /etc/php/$php_ver_num/fpm/php.ini
-	sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/pm.max_children = .*/pm.max_children = 240/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
+	sudo sed -i "s/pm.start_servers = .*/pm.start_servers = 20/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
+	sudo sed -i "s/pm.min_spare_servers = .*/pm.min_spare_servers = 10/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
+	sudo sed -i "s/pm.max_spare_servers = .*/pm.max_spare_servers = 20/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
+	sudo sed -i "s/;pm.max_requests = 500/pm.max_requests = 500/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
+	sudo sed -i "s/output_buffering =.*/output_buffering = 'Off'/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/max_input_time =.*/max_input_time = 3600/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s/post_max_size =.*/post_max_size = 110M/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s/max_file_uploads =.*/max_file_uploads = 100/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s/;session.cookie_secure.*/session.cookie_secure = True/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/output_buffering =.*/output_buffering = 'Off'/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s|max_execution_time =.*|max_execution_time = 360|g" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/max_input_time =.*/max_input_time = 3600/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/post_max_size =.*/post_max_size = 110M/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/upload_max_filesize =.*/upload_max_filesize = 256M/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/max_file_uploads =.*/max_file_uploads = 100/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;session.cookie_secure.*/session.cookie_secure = True/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.enable=.*/opcache.enable=1/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.enable_cli=.*/opcache.enable_cli=1/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.memory_consumption=.*/opcache.memory_consumption=128/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.interned_strings_buffer=.*/opcache.interned_strings_buffer=8/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.max_accelerated_files=.*/opcache.max_accelerated_files=10000/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.revalidate_freq=.*/opcache.revalidate_freq=1/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;opcache.save_comments=.*/opcache.save_comments=1/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;emergency_restart_threshold =.*/emergency_restart_threshold = 10/" /etc/php/$php_ver_num/fpm/php-fpm.conf
+	sudo sed -i "s/;emergency_restart_interval =.*/emergency_restart_interval = 1m/" /etc/php/$php_ver_num/fpm/php-fpm.conf
+	sudo sed -i "s/;process_control_timeout =.*/process_control_timeout = 10s/" /etc/php/$php_ver_num/fpm/php-fpm.conf
+	sudo sed -i "s|allow_url_fopen =.*|allow_url_fopen = On|g" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s|file_uploads =.*|file_uploads = On|g" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s|cgi.fix_pathinfo =.*|cgi.fix_pathinfo=0|g" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/$php_ver_num/cli/php.ini
+	sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL \& ~E_NOTICE \& ~E_STRICT \& ~E_DEPRECATED/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/display_errors = .*/display_errors = Off/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/$php_ver_num/fpm/php.ini
+	sudo sed -i "s/pm.max_children = .*/pm.max_children = 240/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
+	sudo sed -i "s/pm.start_servers = .*/pm.start_servers = 20/" /etc/php/$php_ver_num/fpm/pool.d/www.conf
 	#Tune PHP-FPM pool settings
 	#sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
 	#sed -i "s/;request_terminate_timeout.*/request_terminate_timeout = 60/" /etc/php/7.3/fpm/pool.d/www.conf
@@ -246,18 +179,12 @@ log "Info" "Beginning php.ini edits."
 	#sed -i "s/;pm\.max_requests.*/pm.max_requests = 500/" /etc/php/7.3/fpm/pool.d/www.conf	
 	
 	#Configure sessions directory permissions
-	chmod 733 /var/lib/php/sessions
-	chmod +t /var/lib/php/sessions
+	#chmod 733 /var/lib/php/sessions
+	#chmod +t /var/lib/php/sessions
+	
+	# Restart NGINX
+	sudo service php7.3-fpm restart && sudo service nginx restart
 
-
-    #Run if necessary
-    #create_php_fastcgi_conf
-    
-    #Diable php module
-    sudo a2dismod $phptoinstall
-
-    # Restart Apache
-    sudo service apache2 restart && sudo service $phptoinstall-fpm restart
 log "Info" "Php.ini edits completed."
 }
 
@@ -268,10 +195,10 @@ local phpversion=php7.3
 	if check_sys packageManager apt; then
 		apt_php_package=(
 		php7.3 php7.3-fpm php7.3-common
-		php7.3-cli php7.3-dev php7.3-pgsql php7.3-sqlite3 php7.3-gd php7.3-curl php-memcached 
-		php7.3-imap php7.3-mysql php7.3-mbstring php7.3-xml php-imagick php7.3-zip php7.3-bcmath php7.3-soap 
-		php7.3-intl php7.3-readline php7.3-pspell php7.3-tidy php7.3-xmlrpc php7.3-xsl 
-		php7.3-opcache php-apcu	libapache2-mod-php
+		php7.3-common php7.3-cli php7.3-dev php7.3-pgsql php7.3-sqlite3 php7.3-gd php7.3-curl php-memcached 
+		php7.3-imap php7.3-mbstring php7.3-xml php-imagick php7.3-zip php7.3-bcmath php7.3-soap 
+		php7.3-intl php7.3-readline php7.3-pspell php7.3-tidy php7.3-xmlrpc php7.3-xsl
+		php7.3-opcache php-apcu	
 		)
 		log "Info" "Starting to install primary packages for PHP..."
 		for depend in ${apt_php_package[@]}
@@ -302,18 +229,13 @@ lamp(){
 	log "Info" "Beginning MariaDB install..."
 	install_mariadb
 	log "Info" "MariaDB install completed..."
-
-	log "Info" "Beginning Apache install..."
-	#install_apache_depends	
-	install_apache
-	log "Info" "Apache install completed..."	
-	
+	log "Info" "Beginning Nginx install..."
+	install_nginx
+	log "Info" "MariaDB Nginx completed..."	
 	log "Info" "Beginning PHP install..."
 	#install_php_depends	
 	install_php
 	log "Info" "PHP install completed..."	
-
-
 }
 
 lamp 
