a3d22b6b7f38bc14658e5a9901826f137db2982a
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2019-09-04 11:44:56 -0500

    Update lamp_install.sh

--- static/lamp_install.sh
+++ static/lamp_install.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/wildkatz2004/wordpress-vm/master/lib.sh)
+. <(curl -sL https://raw.githubusercontent.com/wildkatz2004/raspberrypi-wordpress/master/lib.sh)
 unset MYCNFPW
 unset WPDB
 
