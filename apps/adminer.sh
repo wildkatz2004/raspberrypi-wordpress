b3478ec630db071fda76844340befeed40d61f2f
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2019-09-06 17:05:31 -0500

    Update adminer.sh

--- apps/adminer.sh
+++ apps/adminer.sh
@@ -8,7 +8,7 @@ sed -i "s|#precedence ::ffff:0:0/96  100|precedence ::ffff:0:0/96  100|g" /etc/g
 # shellcheck disable=2034,2059
 true
 # shellcheck source=lib.sh
-. <(curl -sL https://raw.githubusercontent.com/techandme/wordpress-vm/master/lib.sh)
+. <(curl -sL https://raw.githubusercontent.com/wildkatz2004/raspberrypi-wordpress/master/lib.sh)
 
 # Check for errors + debug code and abort if something isn't right
 # 1 = ON
