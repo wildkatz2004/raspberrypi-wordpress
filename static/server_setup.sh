9946be0567fb436c3629474ca3c68747f88a8fe2
wildkatz2004 <38220800+wildkatz2004@users.noreply.github.com>
2019-09-06 15:11:00 -0500

    Update server_setup.sh

--- static/server_setup.sh
+++ static/server_setup.sh
@@ -51,12 +51,6 @@ run_static_script adduser
 #    exit
 #fi
 
-# Change DNS
-install_if_not resolvconf
-yes | dpkg-reconfigure --frontend=noninteractive resolvconf
-echo "nameserver 208.67.222.222" > /etc/resolvconf/resolv.conf.d/base
-echo "nameserver 208.67.220.220" >> /etc/resolvconf/resolv.conf.d/base
-
 printf "${Green}Gathering System info${Color_Off}\n" 
 preinstall_lamp
 if [[ "yes" == $(ask_yes_or_no "Check if this is clean server... ?") ]]
@@ -73,6 +67,20 @@ else
 	printf "${Green}OK, moving to next step...${Color_Off}\n" 
 fi
 
+#Install tools
+install_tool
+
+# Change DNS
+yes | dpkg-reconfigure --frontend=noninteractive resolvconf
+echo "nameserver 208.67.222.222" > /etc/resolvconf/resolv.conf.d/base
+echo "nameserver 208.67.220.220" >> /etc/resolvconf/resolv.conf.d/base
+
+sudo ifdown "$IFACE" && sudo ifup "$IFACE"
+if ! nslookup google.com
+then
+    echo "Network NOT OK. You must have a working Network connection to run this script."
+    exit 1
+fi
 
 # Create $SCRIPTS dir
 if [ ! -d "$SCRIPTS" ]
@@ -82,48 +90,101 @@ then
 	log "Info" "Directory created ($SCRIPTS)..."    
 fi
 
-# Check network
-if ! [ -x "$(command -v nslookup)" ]
+print_text_in_color "$ICyan" "Getting scripts from GitHub to be able to run the first setup..."
+
+# All the shell scripts in static (.sh)
+
+download_static_script security
+download_static_script update
+download_static_script test_connection
+download_static_script wp-permissions
+download_static_script change_mysql_pass
+download_static_script techandme
+download_static_script index
+download_le_script activate-ssl
+
+# Make $SCRIPTS excutable
+chmod +x -R "$SCRIPTS"
+chown root:root -R "$SCRIPTS"
+
+# Allow wordpress to run figlet script
+chown wordpress:wordpress $SCRIPTS/techandme.sh
+clear
+
+msg_box"This script will do the final setup for you
+
+- Genereate new server SSH keys
+- Set static IP
+- Create a new WP user
+- Upgrade the system
+- Activate SSL (Let's Encrypt)
+- Install Adminer
+- Change keyboard setup 
+- Change system timezone
+- Set new password to the Linux system (user: wordpress)
+############### D&B Consulting -  $(date +"%Y") ###############"
+
+clear
+# Set keyboard layout
+print_text_in_color "$ICyan" "Current keyboard layout is $(localectl status | grep "Layout" | awk '{print $3}')"
+if [[ "no" == $(ask_yes_or_no "Do you want to change keyboard layout?") ]]
 then
-    apt-get install dnsutils -y -q
-fi
-if ! [ -x "$(command -v ifup)" ]
-then
-    apt-get install ifupdown -y -q
-fi
-sudo ifdown "$IFACE" && sudo ifup "$IFACE"
-if ! nslookup google.com
-then
-    echo "Network NOT OK. You must have a working Network connection to run this script."
-    exit 1
+    print_text_in_color "$ICyan" "Not changing keyboard layout..."
+    sleep 1
+    clear
+else
+    dpkg-reconfigure keyboard-configuration
+clear
 fi
 
-#Changing local timezone to Central Standard
-#sudo timedatectl set-timezone America/Mexico_City
+# Change Timezone
+print_text_in_color "$ICyan" "Current timezone is $(cat /etc/timezone)"
+if [[ "no" == $(ask_yes_or_no "Do you want to change timezone?") ]]
+then
+    print_text_in_color "$ICyan" "Not changing timezone..."
+    sleep 1
+    clear
+else
+    dpkg-reconfigure tzdata
+clear
+fi
+
+# Generate new SSH Keys
+printf "\nGenerating new SSH keys for the server...\n"
+rm -v /etc/ssh/ssh_host_*
+dpkg-reconfigure openssh-server
+
+whiptail --title "Which apps do you want to install?" --checklist --separate-output "Automatically configure and install selected apps\nSelect by pressing the spacebar" "$WT_HEIGHT" "$WT_WIDTH" 4 \
+"Fail2ban" "(Extra Bruteforce protection)   " OFF \
+"Webmin" "(Server GUI)       " OFF \
+"Adminer" "(*SQL GUI)       " OFF 2>results
+while read -r -u 9 choice
+do
+    case $choice in
+        Fail2ban)
+            run_app_script fail2ban
+
+        ;;
+
+        Webmin)
+            run_app_script webmin
+
+        ;;
+
+        Adminer)
+            run_app_script adminer
+        ;;
+
+        *)
+        ;;
+    esac
+done 9< results
+rm -f results
+clear
+
 # Update system
-apt update -q4 & spinner_loading
+apt-get update -q4 & spinner_loading
 
-#Install base packages
-install_base_packages(){
-
-    if check_sys packageManager apt; then
-        apt_depends=(
-		build-essential curl nano wget lftp unzip bzip2 arj nomarch 
-		lzop htop openssl gcc git binutils libmcrypt4 libpcre3-dev make python2.7 
-		python-pip supervisor unattended-upgrades whois zsh imagemagick tcl
-		tree locate software-properties-common screen
-		net-tools ffmpeg ghostscript libfile-fcntllock-perl
-		gnupg2 lsb-release ssl-cert ca-certificates apt-transport-https
-        )
-        log "Info" "Starting to install base packages..."
-        for depend in ${apt_depends[@]}
-        do
-            error_detect_depends "apt-get -y install ${depend}"
-        done
-         log "Info" "Install base packages completed..."
-     fi
-	
-}
 
 if [[ "yes" == $(ask_yes_or_no "Begin installing base packages...?") ]]
 then
@@ -167,16 +228,69 @@ else
 	printf "${Green}OK, moving to next step...${Color_Off}\n" 
 fi
 
+# Define FQDN and create new WP user
+MYANSWER="no"
+while [ "$MYANSWER" == "no" ] 
+do
+   clear
+   cat << ENTERNEW
++-----------------------------------------------+
+|    Please define the FQDN and create a new    |
+|    user for Wordpress.                        |
+|    Make sure your FQDN starts with either     |
+|    http:// or https://, otherwise your        |
+|    installation will not work correctly!      |
++-----------------------------------------------+
+ENTERNEW
+   print_text_in_color "$IGreen" "Enter FQDN (http(s)://yourdomain.com):"
+   read -r FQDN
+   echo
+   print_text_in_color "$IGreen" "Enter username:"
+   read -r USER
+   echo
+   print_text_in_color "$IGreen" "Enter password:"
+   read -r NEWWPADMINPASS
+   echo
+   print_text_in_color "$IGreen" "Enter email address:"
+   read -r EMAIL
+   echo
+   MYANSWER=$(ask_yes_or_no "Is this correct?  FQDN: $FQDN User: $USER Password: $NEWWPADMINPASS Email: $EMAIL") 
+done
+clearecho "$FQDN" > fqdn.txt
+wp_cli_cmd option update siteurl < fqdn.txt --path="$WPATH"
+rm fqdn.txt
+
+OLDHOME=$(wp_cli_cmd option get home --path="$WPATH")
+wp_cli_cmd search-replace "$OLDHOME" "$FQDN" --precise --all-tables --path="$WPATH"
+
+wp_cli_cmd user create "$USER" "$EMAIL" --role=administrator --user_pass="$NEWWPADMINPASS" --path="$WPATH"
+wp_cli_cmd user delete 1 --reassign="$USER" --path="$WPATH"
+{
+echo "WP USER: $USER"
+echo "WP PASS: $NEWWPADMINPASS"
+} > /var/adminpass.txt
+
+# Change servername in Nginx
+server_name=$(echo "$FQDN" | cut -d "/" -f3)
+sed -i "s|# server_name .*|server_name $server_name;|g" $HTTP_CONF
+sed -i "s|# server_name .*|server_name $server_name;|g" $SSL_CONF
+check_command service nginx restart
+
+# Show current administrators
+echo
+print_text_in_color "$ICyan" "This is the current administrator(s):"
+wp_cli_cmd user list --role=administrator --path="$WPATH"
+any_key "Press any key to continue..."
+clear
+
+
+
 #############################################################################
 unattended-upgrades()
 {
 sudo apt-get install unattended-upgrades
 # Following are additional software required
 # we only need mailutils or bsd-mailx, choose 1
-sudo apt-get install mailutils
-sudo apt-get install bsd-mailx
-sudo apt-get install update-notifier-common 
-
 #Setup unattended security upgrades
 cat > /etc/apt/apt.conf.d/50unattended-upgrades << EOF
 Unattended-Upgrade::Origins-Pattern {
@@ -301,16 +415,49 @@ log "Info" "Robot.txt file created..."
 # Prepare for first mount
 download_static_script instruction
 download_static_script history
-#run_static_script change-root-profile
-#run_static_script change-wordpress-profile
-if [ ! -f "$SCRIPTS"/wordpress-startup-script.sh ]
-then
-check_command wget -q "$GITHUB_REPO"/wordpress-startup-script.sh -P "$SCRIPTS"
-fi
 
-# Make $SCRIPTS excutable
-chmod +x -R "$SCRIPTS"
-chown root:root -R "$SCRIPTS"
+# Cleanup
+CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e ''"$(uname -r | cut -f1,2 -d"-")"'' | grep -e '[0-9]' | xargs sudo apt -y purge)
+echo "$CLEARBOOT"
+
+# Cleanup 1
+rm -f "$SCRIPTS/ip.sh"
+rm -f "$SCRIPTS/test_connection.sh"
+rm -f "$SCRIPTS/instruction.sh"
+rm -f "$SCRIPTS/wordpress-startup-script.sh"
+find /root "/home/$UNIXUSER" -type f \( -name '*.sh*' -o -name '*.html*' -o -name '*.tar*' -o -name '*.zip*' \) -delete
+
+# Upgrade system
+print_text_in_color "$ICyan" "System will now upgrade..."
+bash $SCRIPTS/update.sh
+
+# Cleanup 2
+apt-get autoremove -y
+apt-get autoclean
+
+ADDRESS2=$(grep "server_name" /etc/nginx/sites-available/wordpress_port_80.conf | awk '$1 == "server_name" { print $2 }' | cut -d ";" -f1)
+# Success!
+clear
+# Success!
+msg_box "Congratulations! You have successfully installed Wordpress!
+Login to Wordpress in your browser:
+- IP: $ADDRESS
+- Hostname: $(hostname -f)
+
+SUPPORT:
+Please ask for help in the forums, or visit our shop to buy support,
+Email: duane.britting@gmail.com
+
+BUGS:
+Please report any bugs here: https://github.com/wildkatz2004/raspberrypi-wordpress
+
+TIPS & TRICKS:
+1. Publish your server online: https://goo.gl/iUGE2U
+2. To update this VM just type: sudo bash /var/scripts/update.sh
+3. Change IP to something outside DHCP: sudo nano /etc/netplan/01-netcfg.yaml
+
+ ######################### D&B Consulting - $(date +"%Y") #########################  "
+
 
 # Allow wordpress to run theese scripts
 chown www-data:www-data "$SCRIPTS/instruction.sh"
@@ -320,18 +467,11 @@ sudo find /var/www/html/wordpress -type d -exec chmod 755 {} \;
 sudo find /var/www/html/wordpress -type f -exec chmod 644 {} \;
 sudo chown -R www-data /var/www/html/wordpress
 sudo service php"$PHPVER"-fpm restart && sudo service nginx restart
+
 # Upgrade
 apt-get dist-upgrade -y
-
 # Remove LXD (always shows up as failed during boot)
 apt-get purge lxd -y
-
-# Cleanup
-CLEARBOOT=$(dpkg -l linux-* | awk '/^ii/{ print $2}' | grep -v -e ''"$(uname -r | cut -f1,2 -d"-")"'' | grep -e '[0-9]' | xargs sudo apt -y purge)
-echo "$CLEARBOOT"
-apt-get autoremove -y
-apt-get autoclean
-find /root "/home/$UNIXUSER" -type f \( -name '*.sh*' -o -name '*.html*' -o -name '*.tar*' -o -name '*.zip*' \) -delete
 
 # Prefer IPv6
 #sed -i "s|precedence ::ffff:0:0/96  100|#precedence ::ffff:0:0/96  100|g" /etc/gai.conf
@@ -339,4 +479,5 @@ find /root "/home/$UNIXUSER" -type f \( -name '*.sh*' -o -name '*.html*' -o -nam
 # Reboot
 log "Info" "Installation done, system will now reboot..."
 
+sleep 10
 reboot
